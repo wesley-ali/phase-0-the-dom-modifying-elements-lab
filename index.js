e54c818394e5350e7ac631752587c4f0094091eb
Wesley Alinyo <wesleyalinyo@gmail.com>
2024-04-03 12:45:05 +0300

    adds index.js file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code here
+const main = document.querySelector('main#main')
+document.body.removeChild(main)
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Welsey is the champion'
+
+document.body.appendChild(newHeader)
